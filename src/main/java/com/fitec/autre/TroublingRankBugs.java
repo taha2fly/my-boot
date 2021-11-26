8983b508233e45cceea858571acb5ee268486c68
taha2fly <taha.elrobrini@gmail.com>
2021-11-26 16:57:40 +0100

    adding classes

---
+++ src/main/java/com/fitec/autre/TroublingRankBugs.java
@@ -0,0 +1,28 @@
+package com.fitecc.autre;
+
+/**
+* Troubling Rank Bugs Sample.
+*/
+public class TroublingRankBugs {
+
+	/**
+	 * Empty synchronized.
+	 */
+	private void emptySynchronized() {
+		synchronized (this) {
+			// Forgot implementation
+		}
+	}
+
+	/**
+	 * Sleep in synchronized.
+	 * 
+	 * @throws InterruptedException
+	 *             the interrupted exception
+	 */
+	private void sleepInSynchronized() throws InterruptedException {
+		synchronized (this) {
+			Thread.sleep(5000);
+		}
+	}
+}
